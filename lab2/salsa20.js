c51c58d914c726a598aeb965b2579d14a64fafc1
zzzhuk_dev <zhuk.alexey@tabiallc.com>
2022-01-16 15:57:39 +0200

    lab 2: added salsa20 analyzer, cipher

---
+++ lab2/salsa20.js
@@ -0,0 +1,52 @@
+const {bytesToString, stringToBytes, textPercent, hexToBytes} = require('./utils');
+const cipherDecipher = (bytes, key) => {
+  const result = [];
+
+  for (let i = 0; i < bytes.length; i++) {
+    result[i] = bytes[i] ^ key[i % key.length];
+  }
+
+  return result;
+};
+
+const analyzer = (textArr, word) => {
+  let bestPercent = 0, bestText;
+
+  const arrBytes = textArr.map(hexToBytes);
+
+  for (let i = 0; i < arrBytes.length; i++) {
+    const strArr = [];
+
+    for (let j = 0; j < arrBytes.length; j++) {
+      let minLength = Math.min(arrBytes[i].length, arrBytes[j].length);
+
+      let fBytesArr = arrBytes[i].slice(0, minLength - 1);
+      let sBytesArr = arrBytes[j].slice(0, minLength - 1);
+
+      let xorArr = cipherDecipher(fBytesArr, sBytesArr);
+      let wordBytes = stringToBytes(word);
+      let resBytes = cipherDecipher(xorArr, wordBytes);
+      let resStr = bytesToString(resBytes);
+      strArr.push(resStr);
+    }
+
+    const text = '';
+    for(let sIdx = 0; sIdx < strArr.length; sIdx++){
+      let strPart = strArr[sIdx];
+      const minLength = Math.min(strPart.length, word.length);
+      text.concat(strPart.substring(0, minLength))
+    }
+
+    const percent = textPercent(text);
+    if(percent > bestPercent){
+      bestText = text;
+      bestPercent = percent;
+    }
+  }
+
+  return bestText;
+};
+
+module.exports = {
+  analyzer
+}
---
+++ lab2/utils.js
@@ -0,0 +1,33 @@
+module.exports = {
+  bytesToString: (bytes) => {
+    return String.fromCharCode.apply(String, bytes);
+  },
+  stringToBytes: (str) => {
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+      result.push(str.charCodeAt(i));
+    }
+    return result;
+  },
+  hexToBytes: (str) => {
+    let result = [];
+    while (str.length >= 2) {
+      result.push(parseInt(str.substring(0, 2), 16));
+
+      str = str.substring(2, str.length);
+    }
+
+    return result;
+  },
+  textPercent: (str) => {
+    let n = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charAt(i);
+
+      if (/[a-zA-Z,.'"\-:]/.test(char))
+        n++;
+    }
+
+    return n / str.length * 100;
+  }
+};
